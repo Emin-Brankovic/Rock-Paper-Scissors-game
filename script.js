72685781f56f216a29567d36ecc8e83f4ff58b6c
Emin Branković <115090589+Emin-Brankovic@users.noreply.github.com>
2023-05-22 22:28:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
-let userScore=0;
-let pcScore=0;
-let userScore_span=document.getElementById("user-score");
-let pcScore_span=document.getElementById("cp-score");
-const kamen=document.getElementById("kamen");
-const makaze=document.getElementById("makaze");
-const papir=document.getElementById("papir");
-const rezultat=document.getElementById("ispis-pobjede");
+let userScore = 0;
+let pcScore = 0;
+let userScore_span = document.getElementById("user-score");
+let pcScore_span = document.getElementById("cp-score");
+const kamen = document.getElementById("kamen");
+const makaze = document.getElementById("makaze");
+const papir = document.getElementById("papir");
+const rezultat = document.getElementById("ispis-pobjede");
 // funkcija koja nasumicno izabere broj izmedju 1-3 i vraca element niz na odgovarajucem indeksu
-let pcIzbor=()=>{
-    const izbori=["kamen","papir","makaze"];
-    let random=Math.floor(Math.random()*3);
+let pcIzbor = () => {
+    const izbori = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random() * 3);
     return izbori[random];
 }
 
 // glavna game funkcija koja poredi vrijednosti PCIzbora i userInputa te odredjuje ko je pobijednik
-let game=(userIput)=>{
-    const PCIzbor=pcIzbor();
-    if(PCIzbor===userIput){
-        rezultat.innerHTML="Tie";
+let game = (userIput) => {
+    const PCIzbor = pcIzbor();
+    if (PCIzbor === userIput) {
+        rezultat.innerHTML = "Tie";
     }
-    else if(userIput==="kamen"){
-        if(PCIzbor=='papir'){
+    else if (userIput === "Rock") {
+        if (PCIzbor == 'Paper') {
             pcScore++;
-            pcScore_span.innerHTML=pcScore;
-            rezultat.innerHTML="PC won";
+            pcScore_span.innerHTML = pcScore;
+            //rezultat.innerHTML = "You picked " + userIput + " and the PC picked " + PCIzbor + " ,PC won";
+            rezultat.innerHTML =PCIzbor + " beats " + userIput + " ,PC won";
         }
-        else{
+        else {
             userScore++;
-            userScore_span.innerHTML=userScore;
-            rezultat.innerHTML="You won";
-        }
-    }
-    else if(userIput==="makaze"){
-        if(PCIzbor==="kamen"){
-            pcScore++;
-            pcScore_span.innerHTML=pcScore;
-            rezultat.innerHTML="PC won";
-        }
-        else{
-            userScore++;
-            userScore_span.innerHTML=userScore;
-            rezultat.innerHTML="You won";
+            userScore_span.innerHTML = userScore;
+            rezultat.innerHTML =userIput + " beats " + PCIzbor + " ,You won";
         }
     }
-    else if(userIput==="papir"){
-        if(PCIzbor==="makaze"){
+    else if (userIput === "Scissors") {
+        if (PCIzbor === "Rock") {
             pcScore++;
-            pcScore_span.innerHTML=pcScore;
-            rezultat.innerHTML="PC won";
+            pcScore_span.innerHTML = pcScore;
+            rezultat.innerHTML =PCIzbor + " beats " + userIput + " ,PC won";
         }
-        else{
+        else {
             userScore++;
-            userScore_span.innerHTML=userScore;
-            rezultat.innerHTML="You won";
+            userScore_span.innerHTML = userScore;
+            rezultat.innerHTML =userIput + " beat " + PCIzbor + " ,You won";
+        }
+    }
+    else if (userIput === "Paper") {
+        if (PCIzbor === "Scissors") {
+            pcScore++;
+            pcScore_span.innerHTML = pcScore;
+            rezultat.innerHTML =PCIzbor + " beat " + userIput + " ,PC won";
+        }
+        else {
+            userScore++;
+            userScore_span.innerHTML = userScore;
+            rezultat.innerHTML =userIput + " beats " + PCIzbor + " ,You won";
         }
     }
 }
 
-let reset=()=>{
-    pcScore=0;
-    pcScore_span.innerHTML=pcScore;
-    userScore=0;
-    userScore_span.innerHTML=userScore;
-    rezultat.innerHTML="Make your move to start the game";
+let reset = () => {
+    pcScore = 0;
+    pcScore_span.innerHTML = pcScore;
+    userScore = 0;
+    userScore_span.innerHTML = userScore;
+    rezultat.innerHTML = "Make your move to start the game";
 }
